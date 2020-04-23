ce05d2e1c25ce00fe9fe672559c8c6afb4cc5f42
divpaul <divyasujit@hotmail.com>
2020-04-23 09:50:55 -0700

    Refactor with Switch

--- src/main/stgeorge/validator/RequestValidate.java
+++ src/main/stgeorge/validator/RequestValidate.java
@@ -1,94 +1,115 @@
 package stgeorge.validator;
 
-import com.google.gson.internal.$Gson$Preconditions;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RequestValidate {
     private Map<String, String> data;
+    private int minLength = 1;
 
     public RequestValidate(Map<String, String> data) {
         this.data = data;
     }
 
     public RequestValidationResult validateRequest() {
-        boolean isValid = true;
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(" ");
 
         if (data.isEmpty()) {
-            isValid = false;
-            stringBuilder.append(" Request is Empty, ");
+            String message = "Request is Empty";
+            RequestValidationResult result = new RequestValidationResult(false, message);
+            return result;
         }
-        if (data.containsKey("firstname")) {
-            if (data.get("firstname").length() <= 1) {
-                isValid = false;
-                stringBuilder.append(" FirstName is in valid, ");
-            }
-        } else {
-            isValid = false;
-            stringBuilder.append(" FirstName is missing, ");
+        if (!IsRequiredFieldPresent(data)) {
+            String message = "Required field for Request is missing";
+            RequestValidationResult result = new RequestValidationResult(false, message);
+            return result;
         }
 
+        if (data.containsKey("firstname")) {
+            if (data.get("firstname").length() <= minLength) {
+                String message = "FirstName is in valid";
+                RequestValidationResult result = new RequestValidationResult(false, message);
+                return result;
+            }
+        }
 
         if (data.containsKey("lastname")) {
-            if (data.get("lastname").length() <= 1) {
-                isValid = false;
-                stringBuilder.append(" LastName is in valid, ");
+            if (data.get("lastname").length() <= minLength) {
+                String message = "LastName is in valid";
+                RequestValidationResult result = new RequestValidationResult(false, message);
+                return result;
             }
-        } else {
-            isValid = false;
-            stringBuilder.append(" LastName is missing, ");
         }
 
         if (data.containsKey("emailaddress")) {
-            if (data.get("emailaddress").length() <= 4) {
-                isValid = false;
-                stringBuilder.append(" EmailAddress is invalid, ");
-            }
-        } else {
-            isValid = false;
-            stringBuilder.append(" EmailAddress is missing, ");
-        }
-        if (data.containsKey("phonenumber")) {
-            if (data.get("phonenumber").length() < 10) {
-                isValid = false;
-                stringBuilder.append(" PhoneNumber is invalid, ");
-            }
-        } else {
-            isValid = false;
-            stringBuilder.append(" PhoneNumber is missing, ");
-        }
-
-        if (data.containsKey("case") && data.get("case").equals("contactus")) {
-
-            RequestValidationResult validationResult = MessageValidation(data, "ContactUs");
-            isValid = validationResult.getrequestValid();
-            stringBuilder.append(validationResult.getMessage());
-
-
-        }
-
-        if (data.containsKey("case") && data.get("case").equals("qurbananominations")) {
-            if (data.containsKey("services")) {
-                if (data.get("services").length() < 1) {
-                    isValid = false;
-                    stringBuilder.append(" Service is missing for qurbana nominations, ");
+            {
+                String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(data.get("emailaddress"));
+                if (!matcher.matches()) {
+                    String message = "EmailAddress is invalid";
+                    RequestValidationResult result = new RequestValidationResult(false, message);
+                    return result;
                 }
-            } else {
+            }
+        }
+
+        if (data.containsKey("phonenumber")) {
+            {
+                String regex = "^(\\+\\d{1,2}\\s?)?1?\\-?\\.?\\s?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(data.get("phonenumber"));
+                if (!matcher.matches()) {
+                    String message = "PhoneNumber is invalid";
+                    RequestValidationResult result = new RequestValidationResult(false, message);
+                    return result;
+                }
+            }
+        }
+
+        if (data.containsKey("case")) {
+            String caller = data.get("case");
+            switch (caller) {
+                case "contactus":
+                    return ValidateMessage(data, "ContactUs");
+                case "qurbananominations":
+                    return ValidateQurbanaNomination(data);
+                case "perunnalshare":
+                    return ValidatePerunnalShare(data);
+                case "prayerrequest":
+                    return ValidateMessage(data, "Prayer Request");
+                case "onlinegiving":
+                    return ValidateOnlineGiving(data);
+
+            }
+        }
+        RequestValidationResult result = new RequestValidationResult(false, "The Request is for specified case ");
+        return result;
+    }
+
+    private RequestValidationResult ValidateOnlineGiving(Map<String , String> data){
+        boolean isValid = true;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" ");
+        if (data.containsKey("amount")) {
+                if (data.get("amount").length() < 1) {
+                    isValid = false;
+                    stringBuilder.append(" Amount is missing for Online Giving, ");
+                }
+            }
+            else {
                 isValid = false;
-                stringBuilder.append(" Service is missing for qurbana nominations, ");
+                stringBuilder.append(" Amount is missing for Online Giving, ");
             }
 
-            RequestValidationResult validationResult = MessageValidation(data, "Qurbana Nominations");
-            isValid = validationResult.getrequestValid();
-            stringBuilder.append(validationResult.getMessage());
+        RequestValidationResult result = new RequestValidationResult(isValid, stringBuilder.toString());
+        return result;
+    }
 
-
-        }
-        if (data.containsKey("case") && data.get("case").equals("perunnalshare")) {
+    private RequestValidationResult ValidatePerunnalShare(Map<String, String> data){
+        boolean isValid = true;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" ");
             if (data.containsKey("perunnal")) {
                 if (data.get("perunnal").length() < 1) {
                     isValid = false;
@@ -99,39 +120,46 @@ public class RequestValidate {
                 isValid = false;
                 stringBuilder.append(" Perunnal is missing for Perunnal Share request, ");
             }
-            RequestValidationResult validationResult = MessageValidation(data, "Perunnal Share");
+
+            RequestValidationResult validationResult = ValidateMessage(data, "Perunnal Share");
             isValid = validationResult.getrequestValid();
             stringBuilder.append(validationResult.getMessage());
+            RequestValidationResult result = new RequestValidationResult(isValid, stringBuilder.toString());
+            return result;
+    }
+
+    private  RequestValidationResult ValidateQurbanaNomination(Map<String,String> data)
+    {
+        boolean isValid = true;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" ");
 
 
-        }
-
-        if (data.containsKey("case") && data.get("case").equals("prayerrequests")) {
-
-            RequestValidationResult validationResult = MessageValidation(data, "Prayer Requests");
-            isValid = validationResult.getrequestValid();
-            stringBuilder.append(validationResult.getMessage());
-
-
-        }
-        if (data.containsKey("case") && data.get("case").equals("onlinegiving")) {
-            if (data.containsKey("amount")) {
-                if (data.get("amount").length() < 1) {
+            if (data.containsKey("services")) {
+                if (data.get("services").length() < 1) {
                     isValid = false;
-                    stringBuilder.append(" Amount is missing for Online Giving, ");
+                    stringBuilder.append(" Service is missing for qurbana nominations, ");
                 }
             } else {
                 isValid = false;
-                stringBuilder.append(" Amount is missing for Online Giving, ");
+                stringBuilder.append(" Service is missing for qurbana nominations, ");
             }
-        }
 
-        RequestValidationResult result = new RequestValidationResult(isValid, stringBuilder.toString());
-        return result;
+            RequestValidationResult validationResult = ValidateMessage(data, "Qurbana Nominations");
+            isValid = validationResult.getrequestValid();
+            stringBuilder.append(validationResult.getMessage());
+            RequestValidationResult result = new RequestValidationResult(isValid, stringBuilder.toString());
+            return result;
+    }
+
+    private boolean IsRequiredFieldPresent(Map<String, String> data) {
+        if(data.containsKey("firstname") && data.containsKey("lastname") && data.containsKey("emailaddress") &&data.containsKey("phonenumber"))
+            return true;
+        return  false;
     }
 
 
-    private RequestValidationResult MessageValidation(Map<String, String> data, String Case) {
+    private RequestValidationResult ValidateMessage(Map<String, String> data, String Case) {
         boolean isValid = true;
         String message = "";
         if (data.containsKey("message")) {
